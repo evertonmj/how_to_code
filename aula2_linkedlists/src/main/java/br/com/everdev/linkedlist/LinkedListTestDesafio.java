2fc1438a46d54135e66e35fee439b39d4e9a1061
Everton Mendonca Jesus <evertonmj@gmail.com>
2024-09-02 15:49:21 -0300

    commit inicial

---
+++ aula2_linkedlists/src/main/java/br/com/everdev/linkedlist/LinkedListTestDesafio.java
@@ -0,0 +1,35 @@
+package br.com.everdev.linkedlist;
+
+public class LinkedListTestDesafio {
+    public static void main(String[] args) {
+        LinkedListSolucao<String> nomes = new LinkedListSolucao<>();
+        nomes.adicionarNodeInicial("Everton");
+        nomes.adicionarElemento("Joao");
+        nomes.adicionarElemento("Maria");
+        nomes.adicionarElemento("Pedro");
+        nomes.adicionarElemento("Suzana");
+
+        Node<String> raiz = nomes.getHead();
+
+        // Crie um método para impressao da lista com o codigo abaixo
+        while (raiz != null) {
+            System.out.println(raiz.data);
+            raiz = raiz.next;
+        }
+
+        System.out.println("-------------------------");
+
+        // Faça a remoção do ultimo elemento da lista e imprima
+
+        // Faça novamente a remoção do ultimo elemento da lista e imprima
+
+        // Adicione dois elementos na lista e imprima
+
+        // Crie um método para remoção do primeiro elemento da lista e imprima
+    }
+
+    // Crie um método para impressao da lista
+    public static void printLinkedList(Node<String> atual) {
+
+    }
+}
